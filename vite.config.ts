279e422231078cf63bc9c570c1f5c57d0414dd0a
Thierry AGNELLI <thierry.agnelli@gmail.com>
2023-04-29 22:29:28 +0000

    feat: docker config

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: "0.0.0.0",
+    hmr: {
+      clientPort: 9999,
+    },
+    port: 9999,
+    watch: {
+      usePolling: true,
+    },
+  },
+});
