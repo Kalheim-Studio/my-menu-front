edc3eed3f401d20a7b9ec7b263a1be2149338c27
Thierry AGNELLI <thierry.agnelli@gmail.com>
2023-05-08 15:51:46 +0000

    fix: Home page test. Refs: #MYM-19

--- src/pages/Home/Home.test.tsx
+++ src/pages/Home/Home.test.tsx
@@ -1,9 +1,15 @@
 import { render, screen } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+import { Router } from "../../Router/Router";
 import { Home } from "./Home";
 
 describe("Home page component test", () => {
     it("Component should rendering", () => {
-        render(<Home />);
+        render(
+            <BrowserRouter>
+                <Home />
+            </BrowserRouter>
+        );
 
         expect(screen.getByAltText("logo my menu")).toBeDefined();
     });
