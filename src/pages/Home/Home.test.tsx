3f924600e12e431fb8beb83fda3c13af9b910cf8
Thierry AGNELLI <thierry.agnelli@gmail.com>
2023-05-08 16:11:02 +0000

    fix: Build error. Refs: #MYM-19

--- src/pages/Home/Home.test.tsx
+++ src/pages/Home/Home.test.tsx
@@ -1,6 +1,5 @@
 import { render, screen } from "@testing-library/react";
 import { BrowserRouter } from "react-router-dom";
-import { Router } from "../../Router/Router";
 import { Home } from "./Home";
 
 describe("Home page component test", () => {
