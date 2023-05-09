25925c854eb9fd7ae45f0aac7ea579d00a154ab7
Thierry AGNELLI <thierry.agnelli@gmail.com>
2023-05-09 22:50:21 +0000

    feat: Login component wip. Refs: #MYM-20

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,7 +1,11 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import { useAuthenticated } from "../../hooks";
 
 const Login = () => {
+    //  States
+    const [isManager, setIsManager] = useState(false);
+
     // Check if authenticated
     useAuthenticated();
 
@@ -13,13 +17,38 @@ const Login = () => {
             <form>
                 <div className="form-inputs">
                     <label htmlFor="loginMailInput">Email :</label>
+                    <br />
                     <input type="text" id="loginMailInput" name="email" />
                     <div>
-                        <input type="radio" id="ownerRadioInput" name="userRole" value="gerant" />
+                        <input
+                            type="radio"
+                            id="ownerRadioInput"
+                            name="userRole"
+                            value="owner"
+                            defaultChecked
+                            onChange={radioChangeHandler}
+                        />
                         <label htmlFor="ownerRadioInput">Gérant</label>
-                        <input type="radio" id="managerRadioInput" name="userRole" value="manager" />
+                        <input
+                            type="radio"
+                            id="managerRadioInput"
+                            name="userRole"
+                            value="manager"
+                            onChange={radioChangeHandler}
+                        />
                         <label htmlFor="managerRadioInput">Manager</label>
                     </div>
+                    {isManager && (
+                        <>
+                            <label htmlFor="loginManagerIdInput">Identifiant manager :</label>
+                            <br />
+                            <input type="text" id="loginManagerIdInput" name="email" />
+                            <br />
+                        </>
+                    )}
+                    <label htmlFor="loginPwdInput">Mot de passe :</label>
+                    <br />
+                    <input type="text" id="loginPwdInput" name="email" />
                 </div>
                 <div className="form-bottom">
                     <Link to="/register">Pas encore inscrit ?</Link>
@@ -28,6 +57,11 @@ const Login = () => {
             </form>
         </div>
     );
+
+    function radioChangeHandler(e: React.ChangeEvent<HTMLInputElement>) {
+        if (e.target.value === "manager") setIsManager(true);
+        else setIsManager(false);
+    }
 };
 
 export { Login };
