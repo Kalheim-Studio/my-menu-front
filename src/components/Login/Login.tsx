1cbcf7e2c21bac789909f0f1864db8988383e907
Thierry AGNELLI <thierry.agnelli@gmail.com>
2023-05-10 09:46:13 +0000

    feat: Lofin component JSX/CSS. Refs: #MYM-20

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,5 +1,7 @@
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRightToBracket } from "@fortawesome/free-solid-svg-icons";
 import { useAuthenticated } from "../../hooks";
 
 const Login = () => {
@@ -14,45 +16,69 @@ const Login = () => {
             <div className="login-header">
                 <h2>Accès Restaurateur</h2>
             </div>
-            <form>
-                <div className="form-inputs">
-                    <label htmlFor="loginMailInput">Email :</label>
-                    <br />
-                    <input type="text" id="loginMailInput" name="email" />
-                    <div>
+            <form onSubmit={onSubmitHandler}>
+                <div className="login-form-inputs">
+                    <div className="login-form-fragment">
+                        <label htmlFor="loginMailInput">Email :</label>
                         <input
-                            type="radio"
-                            id="ownerRadioInput"
-                            name="userRole"
-                            value="owner"
-                            defaultChecked
-                            onChange={radioChangeHandler}
+                            type="email"
+                            id="loginMailInput"
+                            className="form-input"
+                            name="email"
+                            required
+                            placeholder="john.doe@mail.com"
                         />
-                        <label htmlFor="ownerRadioInput">Gérant</label>
-                        <input
-                            type="radio"
-                            id="managerRadioInput"
-                            name="userRole"
-                            value="manager"
-                            onChange={radioChangeHandler}
-                        />
-                        <label htmlFor="managerRadioInput">Manager</label>
+                        <div>
+                            <input
+                                type="radio"
+                                id="ownerRadioInput"
+                                name="userRole"
+                                value="owner"
+                                defaultChecked
+                                onChange={radioChangeHandler}
+                            />
+                            <label htmlFor="ownerRadioInput" className="login-text-small">
+                Gérant
+                            </label>
+                            <input
+                                type="radio"
+                                id="managerRadioInput"
+                                name="userRole"
+                                value="manager"
+                                onChange={radioChangeHandler}
+                            />
+                            <label htmlFor="managerRadioInput" className="login-text-small">
+                Manager
+                            </label>
+                        </div>
                     </div>
                     {isManager && (
-                        <>
+                        <div className="login-form-fragment">
                             <label htmlFor="loginManagerIdInput">Identifiant manager :</label>
-                            <br />
-                            <input type="text" id="loginManagerIdInput" name="email" />
-                            <br />
-                        </>
+                            <input
+                                type="text"
+                                id="loginManagerIdInput"
+                                className="form-input"
+                                name="managerId"
+                                required
+                                placeholder="JohnDoe"
+                            />
+                        </div>
                     )}
-                    <label htmlFor="loginPwdInput">Mot de passe :</label>
-                    <br />
-                    <input type="text" id="loginPwdInput" name="email" />
+                    <div className="login-form-fragment">
+                        <label htmlFor="loginPwdInput">Mot de passe :</label>
+                        <input type="password" id="loginPwdInput" className="form-input" name="email" required />
+                    </div>
+                    <Link className="login-text-small" to="/reset-password">
+            Problème de connexion ?
+                    </Link>
                 </div>
                 <div className="form-bottom">
                     <Link to="/register">Pas encore inscrit ?</Link>
-                    <input type="submit" value="Entrer" />
+                    {/* <input type="submit" value="Entrer" /> */}
+                    <button>
+            Entrer <FontAwesomeIcon icon={faArrowRightToBracket} />
+                    </button>
                 </div>
             </form>
         </div>
@@ -61,6 +87,11 @@ const Login = () => {
     function radioChangeHandler(e: React.ChangeEvent<HTMLInputElement>) {
         if (e.target.value === "manager") setIsManager(true);
         else setIsManager(false);
+    }
+
+    function onSubmitHandler(e: FormEvent<HTMLFormElement>) {
+        e.preventDefault();
+        console.log(e.target);
     }
 };
 
